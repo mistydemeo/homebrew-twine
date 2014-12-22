73e9fca36ae5e66a847356895fc5af825e62c456
Misty De Meo <mistydemeo@gmail.com>
2014-12-22 15:58:46 -0800

    twine: add HEAD

--- twine.rb
+++ twine.rb
@@ -2,6 +2,7 @@ class Twine < Formula
   homepage "http://twinery.org/"
   url "https://github.com/tweecode/twine/archive/v1.4.2.tar.gz"
   sha1 "8273abc9d14972a7b446d3478ec6bafe1e70a3cf"
+  head "https://github.com/tweecode/twine.git"
 
   depends_on "wxpython"
 
