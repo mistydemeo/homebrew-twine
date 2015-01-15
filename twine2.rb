068558017c99740d13cdf30d641e38636c89713d
Misty De Meo <mistydemeo@gmail.com>
2015-01-14 19:33:57 -0800

    twine2 2.0.2

--- twine2.rb
+++ twine2.rb
@@ -1,7 +1,7 @@
 class Twine2 < Formula
   homepage "http://twinery.org/"
-  url "http://twinery.org/downloads/twine_2.0.1.zip"
-  sha1 "4208ec606622de976fdc8c05f358c1e6e6162f28"
+  url "http://twinery.org/downloads/twine_2.0.2.zip"
+  sha1 "bf242488ed20469f5d4b3f05190c54afac515c87"
 
   def install
     (share/"twine2").install Dir["Twine #{version}/*"]
